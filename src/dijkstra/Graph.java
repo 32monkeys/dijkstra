c611f33e9be2b33a9e9ef43ca19cbb2b0c0d2d52
Lars Bek <38846235+NineNineFive@users.noreply.github.com>
2019-11-18 14:01:57 +0100

    commit dijkstra

--- src/dijkstra/Graph.java
+++ src/dijkstra/Graph.java
@@ -46,6 +46,37 @@ public class Graph {
 
         //implement Dijkstra
 
+        public int[] Dijkstra(int source) {
+        int[] shortestdistance = new int[matrixgraph.length];
+        int[] predecessor = new int[matrixgraph.length];
+        boolean[] handled = new boolean[matrixgraph.length];
+        //Code goes here
+        for (int i = 0; i < matrixgraph.length; i++) {
+            predecessor[i]=-1;
+            handled[i] = false;
+            shortestdistance[i] = (int)Double.POSITIVE_INFINITY;
+        }
+
+        shortestdistance[source - 1] = 0;
+
+        int v, u;
+        int udist, vdist = 200;
+        for (int count = 0; count < matrixgraph.length; count++) {
+            v = findMin(shortestdistance, handled);
+            vdist=shortestdistance[v];
+            System.out.println("Smallest node " + v+ " distance " +shortestdistance[v]);
+            for (int i = 0; i < matrixgraph.length; i++) {
+                if (matrixgraph[v][i] > 0) {
+                    if (matrixgraph[v][i] + vdist < shortestdistance[i]) {
+                        shortestdistance[i] = matrixgraph[v][i] + vdist;
+                        predecessor[i] = v;
+                    }
+                }
+            }
+            handled[v] = true;
+        }
+        return predecessor;
+    }
 
 
         return (new Pair<Integer,Map<Vertex,Vertex>> (DistanceMap.get(zink), PredecessorMap));
