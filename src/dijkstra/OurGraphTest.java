0b2e5f295212173ff833ce96e42615a00f965804
JonatanRytter <JonatanRytter@users.noreply.github.com>
2019-11-19 16:43:55 +0100

    Added graph from portfolio beskrivelse

--- src/dijkstra/OurGraphTest.java
+++ src/dijkstra/OurGraphTest.java
@@ -10,8 +10,8 @@ public class OurGraphTest {
     public static void main(String[] args) {
         // Create graph
         OurGraphTest TestGraph= new OurGraphTest();
-        Graph g = TestGraph.MakeSmallGraph();
-        Vertex source = g.getvertex("A");
+        Graph g = TestGraph.MakeBigGraph();
+        Vertex source = g.getvertex("J");
         Vertex zink = g.getvertex("F");
         Pair<Integer, Map<Vertex, Vertex>> results=g.ShortestDistance(source, zink);
         Vertex current =zink;
@@ -32,30 +32,40 @@ public class OurGraphTest {
 
 
     }
-    public Graph MakeSmallGraph()
-    {
-        Graph mygraph= new Graph();
-        final Vertex A=mygraph.addvertex("A");
-        final Vertex B= mygraph.addvertex("B");
-        final Vertex C =mygraph.addvertex("C");
-        final Vertex D = mygraph.addvertex("D");
-        final Vertex E = mygraph.addvertex("E");
-        final Vertex F = mygraph.addvertex("F");
+    public Graph MakeBigGraph(){
+        Graph ourGraph = new Graph();
+        final Vertex A = ourGraph.addvertex("A");
+        final Vertex B = ourGraph.addvertex("B");
+        final Vertex C = ourGraph.addvertex("C");
+        final Vertex D = ourGraph.addvertex("D");
+        final Vertex E = ourGraph.addvertex("E");
+        final Vertex F = ourGraph.addvertex("F");
+        final Vertex G = ourGraph.addvertex("G");
+        final Vertex H = ourGraph.addvertex("H");
+        final Vertex I = ourGraph.addvertex("I");
+        final Vertex J = ourGraph.addvertex("J");
 
-        mygraph.newedge(A,B,1,2);
-        mygraph.newedge(A,C, 5,1);
-        mygraph.newedge(A,D, 4,6);
-        mygraph.newedge(B,C, 3,2);
-        mygraph.newedge(B,D, 2,3);
-        mygraph.newedge(B,E, 2,4);
-        mygraph.newedge(C,F, 1,8);
-        mygraph.newedge(C,E, 2,2);
-        mygraph.newedge(D,A, 2,7);
-        mygraph.newedge(E,A, 4,7);
-        mygraph.newedge(E,F, 3,6);
-        System.out.println("i ran");
+        ourGraph.newedge(A,B,10,0);
+        ourGraph.newedge(A,D,20,0);
+        ourGraph.newedge(A,E,20,0);
+        ourGraph.newedge(A,F,5,0);
+        ourGraph.newedge(A,G,15,0);
+        ourGraph.newedge(B,C,5,0);
+        ourGraph.newedge(B,D,10,0);
+        ourGraph.newedge(C,B,15,0);
+        ourGraph.newedge(C,D,5,0);
+        ourGraph.newedge(D,E,10,0);
+        ourGraph.newedge(E,F,5,0);
+        ourGraph.newedge(G,F,10,0);
+        ourGraph.newedge(H,A,5,0);
+        ourGraph.newedge(H,B,20,0);
+        ourGraph.newedge(H,G,5,0);
+        ourGraph.newedge(I,B,15,0);
+        ourGraph.newedge(I,H,20,0);
+        ourGraph.newedge(I,J,10,0);
+        ourGraph.newedge(J,B,5,0);
+        ourGraph.newedge(J,C,15,0);
 
-        return mygraph;
-
+        return ourGraph;
     }
 }
